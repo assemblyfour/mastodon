fd2631f604bf94d55a99d016eaeeb75fb0fb4130
chendo <chendo@users.noreply.github.com>
2018-04-25 14:06:46 +1000

    Improve SWValidator

--- app/validators/swlistings_validator.rb
+++ app/validators/swlistings_validator.rb
@@ -3,23 +3,21 @@
 class SwlistingsValidator < ActiveModel::Validator
   def validate(status)
     return unless status.local? && !status.reblog?
+    return if status.reply? || status.private_visibility? || status.direct_visibility?
+    return if status.account.user.staff?
 
     tags = Extractor.extract_hashtags(status.text)
 
     return unless tags.map(&:downcase).include? 'swlisting'
-    return if status.reply? || status.private_visibility? || status.direct_visibility?
-    return if status.account.user.staff?
 
     unless status.account.avatar?
       status.errors.add(:base, 'You need a profile picture to post on the #swlisting hashtag')
     end
 
-    swlisting_statuses = ::Status.tagged_with(Tag.find_by(name: 'swlisting'))
-                                .where(account: status.account)
-                                .local
+    swlisting_statuses = status.account.statuses
+                                .tagged_with(Tag.find_by(name: 'swlisting'))
                                 .without_replies
                                 .without_reblogs
-                                .excluding_silenced_accounts
                                 .where(Status.arel_table[:created_at].gt(1.day.ago))
 
     unless swlisting_statuses.count < 3
