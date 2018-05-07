22198bc7024eedf422998661ec4c1266c11ffe38
chendo <chendo@users.noreply.github.com>
2018-05-07 14:43:54 +1000

    Wrap redis in a connection pool

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-redis_connection = Redis.new(
-  url: ENV['REDIS_URL'],
-  driver: :hiredis
-)
+redis_connection = ConnectionPool::Wrapper.new(size: ENV.fetch('MAX_THREADS').to_i, timeout: 3) do
+  Redis.new(
+   url: ENV['REDIS_URL'],
+    driver: :hiredis
+  )
+end
 
 namespace = ENV.fetch('REDIS_NAMESPACE') { nil }
 
