42d9def0bbf3e6a1e446134d4d92c77cf429617e
chendo <chendo@users.noreply.github.com>
2018-05-19 21:27:30 +1000

    Cache the models

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -6,20 +6,15 @@ class PopularController < ApplicationController
   FAVOURITE_WEIGHTING = 1
   CONVERSATION_WEIGHTING = 2
   def index
-    @popular = Status
-                .local
-                .includes(:account, :preview_cards, :mentions, :conversation)
-                .with_public_visibility
-                .where('reblogs_count + favourites_count > ?', threshold)
-                .where('created_at BETWEEN ? AND ?', page.days.ago, (page - 1).days.ago)
-                .reorder('reblogs_count + favourites_count DESC')
-                .limit(100)
-                .sort_by { |s|
-                  s.reblogs_count * REBLOG_WEIGHTING +
-                  s.favourites_count * FAVOURITE_WEIGHTING +
-                  s.conversation.statuses.without_reblogs.count * CONVERSATION_WEIGHTING
-                }
-                .reverse[0...25]
+    @popular = Rails.cache.fetch("popular/#{page}", expires_in: 5.minutes, race_condition_ttl: 10.seconds) do
+      Status.local
+            .includes(:account, :preview_cards, :mentions, :conversation)
+            .with_public_visibility
+            .where('reblogs_count + favourites_count > ?', threshold)
+            .where('created_at BETWEEN ? AND ?', page.days.ago, (page - 1).days.ago)
+            .reorder('reblogs_count + favourites_count DESC')
+            .limit(100)
+    end
     @page = page
   end
 
