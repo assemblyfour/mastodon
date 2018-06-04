d4c00f6dab735acc138988e135dfd29a2f2d9de1
chendo <chendo@users.noreply.github.com>
2018-06-04 14:28:28 +1000

    Track users that have interacted and tooted

--- app/workers/scheduler/stats_scheduler.rb
+++ app/workers/scheduler/stats_scheduler.rb
@@ -34,10 +34,19 @@ class Scheduler::StatsScheduler
       b.gauge('statuses.toots', Status.local.without_replies.without_reblogs.count)
       b.gauge('statuses.total', Status.local.count)
       b.gauge('users.active.day', User.where('current_sign_in_at > ?', 1.day.ago).count)
-      b.gauge('users.active.month', User.where('current_sign_in_at > ?', 1.month.ago).count)
       b.gauge('users.active.week', User.where('current_sign_in_at > ?', 1.week.ago).count)
+      b.gauge('users.active.month', User.where('current_sign_in_at > ?', 1.month.ago).count)
       b.gauge('users.count', User.confirmed.count, tags: ["state:confirmed"])
       b.gauge('users.count', User.where(confirmed_at: nil).count, tags: ["state:unconfirmed"])
+
+      ActiveRecord::Base.connection.select_all("SELECT count(distinct(account_id)) FILTER (WHERE created_at > now() - interval '24 hours') as day, count(distinct(account_id)) FILTER (WHERE created_at > now() - interval '7 days') as week, count(distinct(account_id)) FILTER (WHERE created_at > now() - interval '30 days') as month FROM statuses WHERE local = true").to_hash.first.each do |k, v|
+        b.gauge("users.interacted.#{k}", v)
+      end
+
+      ActiveRecord::Base.connection.select_all("SELECT count(distinct(account_id)) FILTER (WHERE created_at > now() - interval '24 hours') as day, count(distinct(account_id)) FILTER (WHERE created_at > now() - interval '7 days') as week, count(distinct(account_id)) FILTER (WHERE created_at > now() - interval '30 days') as month FROM statuses WHERE local = true AND reply = false AND reblog_of_id IS NULL ").to_hash.first.each do |k, v|
+        b.gauge("users.tooted.#{k}", v)
+      end
+
     end
   end
 
