37ac10300b8ea89e211c723b5011922b849bf5da
chendo <chendo@users.noreply.github.com>
2018-08-03 16:59:00 +1000

    Add some metadata to help people understand suspicious posts

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,6 +5,7 @@ class ListingsController < ApplicationController
   layout 'public'
 
   def index
+    @swlisting_tag = Tag.find_by(name: 'swlisting')
     results = ListingSearchService.new.call(params[:query])
     @listings = Status.includes(:media_attachments, :mentions, :stream_entry, :account).where(Status.arel_table[:id].in(results[:listings].map(&:id)))
   end
