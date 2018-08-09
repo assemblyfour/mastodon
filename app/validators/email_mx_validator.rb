6328f753ecd5c0f3599a5ef33b5a667e1eafa0ed
chendo <chendo@users.noreply.github.com>
2018-08-09 15:25:10 +1000

    MXValidator checks against IP also on blacklist

--- app/validators/email_mx_validator.rb
+++ app/validators/email_mx_validator.rb
@@ -16,6 +16,10 @@ class EmailMxValidator < ActiveModel::Validator
     return true if domain.nil?
 
     records = Resolv::DNS.new.getresources(domain, Resolv::DNS::Resource::IN::MX).to_a.map { |e| e.exchange.to_s }
+    records += records.map do |record|
+      Resolv::DNS.new.getresources(record, Resolv::DNS::Resource::IN::A).to_a.map { |e| e.address.to_s }
+    end
+    records.flatten!
     records.empty? || on_blacklist?(records)
   end
 
