6e3b628a060626da718d0f17ce4846c569a6bfde
chendo <chendo@users.noreply.github.com>
2018-11-21 13:51:00 +1100

    Check against number of reports from that IP address and block after 2 or more reports with keywords

--- app/validators/swlistings_validator.rb
+++ app/validators/swlistings_validator.rb
@@ -10,9 +10,17 @@ class SwlistingsValidator < ActiveModel::Validator
 
     return unless tags.map(&:downcase).include? 'swlisting'
 
+    if status.account.created_at > 1.week.ago
+      status.errors.add(:base, "Your account must be older than a week to post on the #swlisting hashtag")
+      return
+    end
+
     unless status.account.avatar?
       status.errors.add(:base, 'You need a profile picture to post on the #swlisting hashtag')
+      return
     end
+
+    ips = [status.account.current_sign_in_ip, status.account.last_sign_in_ip].compact.uniq
 
     swlisting_statuses = status.account.statuses.
                                 tagged_with(Tag.find_by(name: 'swlisting')).
@@ -21,7 +29,16 @@ class SwlistingsValidator < ActiveModel::Validator
                                 where(Status.arel_table[:updated_at].gt(1.day.ago))
 
     unless swlisting_statuses.count { |s| s.text =~ /#swlisting/ } < 3
-      status.errors.add(:base, 'You can only post 3 times per day to #swlisting')
+      return status.errors.add(:base, 'You can only post 3 times per day to #swlisting')
+    end
+
+    if Report.joins(target_account: [:user]).
+      where('current_sign_in_ip IN (?) OR last_sign_in_ip IN (?)', ips, ips).
+      where('comment ~* ?', '.*(fake|spam|scam).*').
+      distinct(:account_id).
+      count >= 2
+      status.errors.add(:base, "Your IP has been blocked for spam.")
+      return
     end
   end
 end
